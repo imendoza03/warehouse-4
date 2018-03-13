9c8d6d3fcf56d30294fa6693bccebf6f244a6256
imendoza03 <ivanmendoza@gmail.com>
2018-03-13 09:52:27 +0100

    sql queries

---
+++ sqlQueries.sql
@@ -0,0 +1,23 @@
+/*Create database Warehouse4*/
+CREATE DATABASE IF NOT EXISTS warehouse4 DEFAULT CHARACTER SET utf8 COLLATE = utf8_bin;
+
+/*Create tables Warehouse4*/
+CREATE TABLE `warehouse4` . `user` (
+	id int unsigned auto_increment primary key,
+    first_name varchar(255),
+    last_name varchar(255),
+    user_name varchar(255),
+	`password` varchar(255)
+);
+
+CREATE TABLE `warehouse4` . `stock` (
+	id int unsigned auto_increment primary key,
+    product_name varchar(255),
+    description varchar(255),
+    amount varchar(255),
+    creation_date datetime,
+	update_date datetime
+)
+engine = InnoDB
+default character set  = utf8
+collate = utf8_bin;
