fee6d34b511e4e79898e32baf78c9d8572c72dc3
imendoza0315 <mendoza.ivaneladio@gmail.com>
2018-03-13 21:10:45 +0100

    corrections

--- src/Model/User.php
+++ src/Model/User.php
@@ -11,7 +11,6 @@ class User {
     protected $lastName;
     protected $username;
     protected $password;
-<<<<<<< HEAD
     protected $connection;
     
     public function __construct(){
@@ -25,9 +24,6 @@ class User {
         }
         
     }
-=======
-    
->>>>>>> e9fa5213a0707a40443d09ba508cefd34114a0aa
     
     /**
      * @return mixed
